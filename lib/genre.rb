f968c9c6a3126dc1c37d8a2f72caee1ea006cee4
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-02 03:24:09 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,9 +1,6 @@
 class Genre
-<<<<<<< HEAD
   attr_accessor :name, :songs, :artist
-=======
   attr_accessor :name, :songs
->>>>>>> 2c6fec5977fd0cbede4d1f6129ddddb26bbd0a77
   @@all = []
 
   def initialize(name)
@@ -16,15 +13,12 @@ class Genre
   end
 
   def songs
-<<<<<<< HEAD
     Song.all.each { |song| song.genre ==self }
   end
 
   def artists
     songs.map {|song| song.artist }
-=======
     songs.each { |song| song.genre  }
->>>>>>> 2c6fec5977fd0cbede4d1f6129ddddb26bbd0a77
   end
 
 end
